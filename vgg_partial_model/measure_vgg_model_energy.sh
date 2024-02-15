7d2f320240a32687210565b901cbaa8a9b627649
dietrich <marc.dietrich@mailbox.tu-dresden.de>
2024-02-15 12:35:23 +0100

    dse_adjustments

--- vgg_partial_model/measure_vgg_model_energy.sh
+++ vgg_partial_model/measure_vgg_model_energy.sh
@@ -12,10 +12,10 @@ cpu-energy-meter -r &
 PID=$!
 
 # Call the Python script with parameters
-output=$(python eval_saved_model.py --core_affinities "$CORE_AFFINITIES")
+output=$(python eval_saved_model.py --core_affinities $CORE_AFFINITIES)
 
 
-captured_line_exec_time = $(echo "$output" | grep "exec_time")
+captured_line_exec_time=$(echo "$output" | grep "exec_time")
 echo "$captured_line_exec_time"
 
 # Send SIGINT signal to cpu-energy-meter
